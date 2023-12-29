1ab7b0530cfbce379355c4a02f7183ff72b17e62
JustAbdu11ah <144443430+JustAbdu11ah@users.noreply.github.com>
2023-12-29 23:31:15 +0500

    final

---
+++ src/AppContainer.js
@@ -0,0 +1,26 @@
+import './App.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar'; 
+import Calculator from './components/Calculator';
+import Quote from './components/Quote';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/about" element={<Quote />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+export default App;
---
+++ src/components/HomeComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h2 className='container-fluid '>Welcom to Math Magician</h2>
+    </div>
+  )
+}
+
+export default Home
